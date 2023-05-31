58800751e8194b23fd4b9b3ddcdbf165cbb34f0e
Joao Claudio Mendonca Pansani <pansanijoaoclaudio@gmail.com>
2023-05-31 12:26:33 -0300

    passed data from ExpenseForm to App.js

--- src/App.js
+++ src/App.js
@@ -24,9 +24,13 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+
+  }
+
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses items={expenses}/>
     </div>
   );
